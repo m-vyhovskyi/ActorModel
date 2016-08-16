6e8940589a5e8db8a7bbf4a3eacba9e2dab70d1f
Maksym Vyhovskyi <m.vyhovskyi@evision-software.com>
2016-08-16 17:14:25 +0200

    The first test added

---
+++ ActorModel.Tests/StatisticsActorTests.cs
@@ -0,0 +1,15 @@
+﻿using Akka.TestKit.Xunit2;
+
+using Xunit;
+
+namespace ActorModel.Tests
+{
+    public class StatisticsActorTests : TestKit
+    {
+        [Fact(DisplayName = "The number of plays initially equals 0")]
+        public void ShouldHaveInitialPlayCountsValue()
+        {
+            
+        }
+    }
+}
