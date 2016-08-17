de2e0a0451bee4ade2af51952ddf81d4096bb4ac
Maksym Vyhovskyi <m.vyhovskyi@evision-software.com>
2016-08-17 17:21:44 +0200

    Mocking non actor dependencies

---
+++ ActorModel.Tests/DatabaseActorTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+using Akka.Actor;
+using Akka.TestKit.Xunit2;
+
+using Moq;
+
+using Xunit;
+
+namespace ActorModel.Tests
+{
+    public class DatabaseActorTests : TestKit
+    {
+        private const string BooleanLies = "Boolean Lies";
+        private const string CodenanTheBarbarian = "Codenan the Barbarian";
+
+        [Fact]
+        public void ShouldReadStatsFromDatabase()
+        {
+            var statsData = new Dictionary<string, int>
+            {
+                { BooleanLies, 42 },
+                { CodenanTheBarbarian, 200 }
+            };
+
+            var mockDb = new Mock<IDatabaseGateway>();
+            mockDb.Setup(x => x.GetStoredStatistics()).Returns(statsData);
+
+            IActorRef actor = ActorOf(Props.Create(() => new DatabaseActor(mockDb.Object)));
+
+            actor.Tell(new GetInitialStatisticsMessage());
+
+            var received = ExpectMsg<InitialStatisticsMessage>();
+
+            Assert.Equal(received.PlayCounts[BooleanLies], 42);
+            Assert.Equal(received.PlayCounts[CodenanTheBarbarian], 200);
+        }
+    }
+}
---
+++ ActorModel/DatabaseActor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+using Akka.Actor;
+
+namespace ActorModel
+{
+    public class DatabaseActor : ReceiveActor
+    {
+        private readonly IDatabaseGateway databaseGateway;
+
+        public DatabaseActor(IDatabaseGateway databaseGateway)
+        {
+            this.databaseGateway = databaseGateway;
+
+            Receive<GetInitialStatisticsMessage>(message =>
+            {
+                var storedStats = databaseGateway.GetStoredStatistics();
+
+                Sender.Tell(new InitialStatisticsMessage(new ReadOnlyDictionary<string, int>(storedStats)));
+            });
+        }
+    }
+}
